691b120b8d08852367f3c19d4dc1ae600ad29e94
Gwenael_Cruz <cruz.gwenael@gmail.com>
2017-03-16 09:36:22 +0100

    Ajout des controllers pour les followers et du model, et routes nested

---
+++ app/controllers/followers_controller.rb
@@ -0,0 +1,75 @@
+class FollowersController < ApplicationController
+  before_action :set_user
+
+  # GET /users
+  # GET /users.json
+  def index
+    @follows = Follow.where.not('follower_id', @user.id)
+  end
+
+  # GET /users/1
+  # GET /users/1.json
+  def show
+  end
+
+  # GET /users/new
+  def new
+    @follow = Follow.new
+  end
+
+  # GET /users/1/edit
+  def edit
+  end
+
+  # POST /users
+  # POST /users.json
+  def create
+    @follow = Follow.new(follow_params)
+    @user = User.find(1)
+    respond_to do |format|
+      if @follow.save
+        format.html { redirect_to @user, notice: 'Followed was successfully created.' }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { redirect_to @user, notice: 'Follow could not be created'}
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /users/1
+  # PATCH/PUT /users/1.json
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /users/1
+  # DELETE /users/1.json
+  def destroy
+    @follow.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+
+    def set_user
+      @user = User.find(params[:user_id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:name, :img_url)
+    end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   validates :name, presence: true,
             length: { minimum: 3, maximum: 20},
             uniqueness: true
-
+  has_many :followers, :foreign_key => 'follower_id', :class_name => 'follow'
+  has_many :followed, :foreign_key => 'followed_id', :class_name => 'follow'
 
 end
